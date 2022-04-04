8434b9788e7cd5fbf1a609dd6f2bb51256859edd
Garrett Franklin <garrettfranklin@pm.me>
2022-04-04 09:22:52 -0600

    Create display functions
    Create functions to update the display. Build functionality for number
    buttons and operator buttons. Equals and Clear not defined yet.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+// Set up global variables
+let displayValue = "";
+let currentTotal = null;
+let nextOperator = null;
+
 // Define operator functions
 function add(a, b) {
   return a + b;
@@ -9,7 +14,14 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
- return a / b;
+  if (b === 0) return "Can't divide by zero, dumbass.";
+  return a / b;
+}
+const operators = {
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
 }
 
 // Operate function that takes an operator function and two numbers
@@ -17,12 +29,44 @@ function operate(operator, a, b) {
   return operator(a, b);
 }
 
-// Create number buttons
-const numberContainer = document.querySelector(".numbercontainer");
+// Create buttons
+const buttonContainer = document.querySelector(".buttoncontainer");
+// Number buttons
 for (let i = 0; i < 10; i++) {
   const button = document.createElement("button");
   button.setAttribute("class", "numberbutton");
   button.setAttribute("id", i);
   button.textContent = i;
-  numberContainer.appendChild(button);
+  buttonContainer.appendChild(button);
 }
+// Functions for updating the display
+const display = document.querySelector(".display");
+function updateDisplay(number) {
+  // Display value is a string, so + concats the number to the end
+  displayValue = displayValue + number;
+  display.textContent = displayValue;
+}
+function updateNextOperator(operator) {
+  if (!nextOperator) {
+    currentTotal = Number(displayValue);
+    displayValue = "";
+    nextOperator = operators[operator];
+    console.log(currentTotal, displayValue, nextOperator);
+  } else {
+    currentTotal = operate(nextOperator, currentTotal, Number(displayValue));
+    nextOperator = operators[operator];
+    displayValue = "";
+    display.textContent = currentTotal;
+  }
+}
+
+// Link display functions to buttons
+const numberButtons = document.querySelectorAll(".numberbutton");
+numberButtons.forEach(button => {
+  button.addEventListener("click", e => updateDisplay(e.target.id));
+})
+
+const operatorButtons = document.querySelectorAll(".operatorbutton");
+operatorButtons.forEach(button => {
+  button.addEventListener("click", e => updateNextOperator(e.target.id));
+})
